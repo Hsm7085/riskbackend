1fc29d8746a42c1b2fc118ce51653d76f21fd960
Nazir Ansari <naziransari989@gmail.com>
2023-03-17 13:48:49 +0530

    updated backend and frontend

--- backend/connection/db.js
+++ backend/connection/db.js
@@ -4,7 +4,7 @@ const connection =mysql.createConnection({
     host:'localhost',
     user:'root',
     database:'riskprofile',
-    password: 'himanshu'
+    password: 'naziransari'
 });
 connection.connect(function(err){
     if(err) throw err;
